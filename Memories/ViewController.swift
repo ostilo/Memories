e212d70de4cb320977da420a16595321396ff549
GWINA <babsgodwin@gmail.com>
2021-06-30 17:05:04 +0100

    Initial Commit

---
+++ Memories/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Memories
+//
+//  Created by ELKANAHTECH_02 on 30/06/2021.
+//  Copyright © 2021 ELKANAHTECH_02. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
